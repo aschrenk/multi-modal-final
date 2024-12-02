7c35f25bd7e072224756f7c18b4a134c7b35fb1c
aschrenk <ma040360@ucf.edu>
2024-12-01 23:57:59 -0500

    final updates

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -317,6 +318,10 @@ public class GameManager : MonoBehaviour
             RouteSplit();
             PlayLine();
             return;
+        }
+        else if (currentLine == 64)
+        {
+            SceneManager.LoadScene("Create-with-VR-Starter-Scene");
         }
         //end scene 1, setup scene 2 (3 for demo)
         else if (currentLine == 17)
